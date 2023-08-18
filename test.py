de7b63520da619a8f463aac22c107ee493965749
Dustin Born <dustin.born@gmx.de>
2023-08-19 00:21:35 +0200

    Update the test cases to only check the best match
    cpe_search is only deterministic in all of its results if the CPE
    dict is in the same order. With the new building mechanism via the
    NVD's API, the CPE dict is not always in the same order. Therefore,
    the only part of the result that is guaranteed to be deterministic
    now is the best match.

--- test.py
+++ test.py
@@ -7,48 +7,84 @@ class TestSearches(unittest.TestCase):
 
     def test_search_wp_572(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='WordPress 5.7.2'))
-        self.assertEqual(result, "{'WordPress 5.7.2': [('cpe:2.3:a:wordpress:wordpress:5.7.2:*:*:*:*:*:*:*', 0.9805804786431419), ('cpe:2.3:a:wordpress:wordpress:-:*:*:*:*:*:*:*', 0.7071067811865475), ('cpe:2.3:a:adenion:blog2social:5.7.2:*:*:*:*:wordpress:*:*', 0.6859944446591075)]}")
+        query = 'WordPress 5.7.2'
+        test_best_match_cpe = 'cpe:2.3:a:wordpress:wordpress:5.7.2:*:*:*:*:*:*:*'
+        test_best_match_score = '0.9805804786431419'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_apache_2425(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Apache 2.4.25'))
-        self.assertEqual(result, "{'Apache 2.4.25': [('cpe:2.3:a:apache:http_server:2.4.25:*:*:*:*:*:*:*', 0.737210014936463), ('cpe:2.3:a:apache:apache-airflow-providers-apache-spark:-:*:*:*:*:*:*:*', 0.600000153741923), ('cpe:2.3:a:apache:apache-airflow-providers-apache-hive:-:*:*:*:*:*:*:*', 0.600000153741923)]}")
+        query = 'Apache 2.4.25'
+        test_best_match_cpe = 'cpe:2.3:a:apache:http_server:2.4.25:*:*:*:*:*:*:*'
+        test_best_match_score = '0.737210014936463'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_proftpd_133c(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Proftpd 1.3.3c'))
-        self.assertEqual(result, "{'Proftpd 1.3.3c': [('cpe:2.3:a:proftpd:proftpd:1.3.3:c:*:*:*:*:*:*', 0.8333337218752717), ('cpe:2.3:a:proftpd:proftpd:1.3.3:rc1:*:*:*:*:*:*', 0.700140318058465), ('cpe:2.3:a:cauldrondevelopment:c\\\!:1.3.3:*:*:*:*:*:*:*', 0.6666664001499099)]}")
+        query = 'Proftpd 1.3.3c'
+        test_best_match_cpe = 'cpe:2.3:a:proftpd:proftpd:1.3.3:c:*:*:*:*:*:*'
+        test_best_match_score = '0.8333337218752717'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_thingsboard_341(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Thingsboard 3.4.1'))
-        self.assertEqual(result, "{'Thingsboard 3.4.1': [('cpe:2.3:a:thingsboard:thingsboard:3.4.1:*:*:*:*:*:*:*', 0.9805804786431419), ('cpe:2.3:a:thingsboard:thingsboard:1.0:*:*:*:*:*:*:*', 0.6324555256696553), ('cpe:2.3:a:mathjs:math.js:3.4.1:*:*:*:*:*:*:*', 0.4714045207910316)]}")
+        query = 'Thingsboard 3.4.1'
+        test_best_match_cpe = 'cpe:2.3:a:thingsboard:thingsboard:3.4.1:*:*:*:*:*:*:*'
+        test_best_match_score = '0.9805804786431419'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_redis_323(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Redis 3.2.3'))
-        self.assertEqual(result, "{'Redis 3.2.3': [('cpe:2.3:a:redis:redis:3.2.3:*:*:*:*:*:*:*', 0.9805804786431419), ('cpe:2.3:a:redislabs:redis:3.2.3:*:*:*:*:*:*:*', 0.9128707750749131), ('cpe:2.3:a:pivotal_software:redis:3.2.3:*:*:*:*:*:*:*', 0.7071067811865475)]}")
+        query = 'Redis 3.2.3'
+        test_best_match_cpe = 'cpe:2.3:a:redis:redis:3.2.3:*:*:*:*:*:*:*'
+        test_best_match_score = '0.9805804786431419'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_piwik_045(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Piwik 0.4.5'))
-        self.assertEqual(result, "{'Piwik 0.4.5': [('cpe:2.3:a:piwik:piwik:0.4.5:*:*:*:*:*:*:*', 0.9805804786431419), ('cpe:2.3:a:piwik:piwik:0.2.32:*:*:*:*:*:*:*', 0.5883482871858852), ('cpe:2.3:a:bitcoin:bitcoind:0.4.5:*:*:*:*:*:*:*', 0.4714045207910316)]}")
+        query = 'Piwik 0.4.5'
+        test_best_match_cpe = 'cpe:2.3:a:piwik:piwik:0.4.5:*:*:*:*:*:*:*'
+        test_best_match_score = '0.9805804786431419'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_vmware_spring_framework_5326(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='VMWare Spring Framework 5.3.26'))
-        self.assertEqual(result,"{'VMWare Spring Framework 5.3.26': [('cpe:2.3:a:vmware:spring_framework:5.3.26:*:*:*:*:*:*:*', 1.0), ('cpe:2.3:a:vmware:spring_framework:4.3.30:*:*:*:*:*:*:*', 0.75), ('cpe:2.3:a:laravel:framework:5.3.26:*:*:*:*:*:*:*', 0.5773500383793437)]}")
+        query = 'VMWare Spring Framework 5.3.26'
+        test_best_match_cpe = 'cpe:2.3:a:vmware:spring_framework:5.3.26:*:*:*:*:*:*:*'
+        test_best_match_score = '1.0'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_zulip_server_48(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Zulip server 4.8'))
-        self.assertEqual(result, "{'Zulip server 4.8': [('cpe:2.3:a:zulip:zulip:4.8:*:*:*:*:*:*:*', 0.8006406081365984), ('cpe:2.3:a:zulip:zulip_server:1.3.0:*:*:*:*:*:*:*', 0.7001398086317259), ('cpe:2.3:a:zulip:zulip_server:2021-06-03:*:*:*:*:*:*:*', 0.6123723544509724)]}")
+        query = 'Zulip server 4.8'
+        test_best_match_cpe = 'cpe:2.3:a:zulip:zulip:4.8:*:*:*:*:*:*:*'
+        test_best_match_score = '0.8006406081365984'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
     def test_search_electron_1317(self):
         self.maxDiff = None
-        result = str(search_cpes(queries='Electron 13.1.7'))
-        self.assertEqual(result, "{'Electron 13.1.7': [('cpe:2.3:a:electronjs:electron:13.1.7:*:*:*:*:*:*:*', 0.8164962545126949), ('cpe:2.3:a:markdown-electron_project:markdown-electron:-:*:*:*:*:*:*:*', 0.4714045207910316), ('cpe:2.3:a:jgraph:drawio:13.1.7:*:*:*:*:*:*:*', 0.44721383028334494)]}")
+        query = 'Electron 13.1.7'
+        test_best_match_cpe = 'cpe:2.3:a:electronjs:electron:13.1.7:*:*:*:*:*:*:*'
+        test_best_match_score = '0.8164962545126949'
+        result = search_cpes(queries=[query])
+        self.assertEqual(result[query][0][0], test_best_match_cpe)
+        self.assertEqual(str(result[query][0][1]), test_best_match_score)
 
 if __name__ == '__main__':
     unittest.main()
